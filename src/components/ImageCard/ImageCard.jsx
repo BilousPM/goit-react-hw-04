30f6b1900892b1ef64e2dff18b65d4be893f4218
BilousPM <bilouspm@gmail.com>
2024-07-26 12:09:26 -0600

    adds some information about author

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,17 +1,24 @@
 import s from "./ImageCard.module.css";
 
 const ImageCard = ({ data, onClick }) => {
+  console.log(data);
   return (
     <>
-      <img
-        className={s.image}
-        src={data.urls.small}
-        alt=""
-        width={350}
-        height={300}
-        onClick={() => onClick(data.urls.regular)}
-        style={{ cursor: "pointer" }}
-      />
+      <div>
+        <img
+          className={s.image}
+          src={data.urls.small}
+          alt={data.alt_description}
+          width={350}
+          height={300}
+          onClick={() => onClick(data.urls.regular)}
+          style={{ cursor: "pointer" }}
+        />
+        <p className={s.likes}>{`❤️ ${data.likes}`}</p>
+        <a href={data.user.links.html} className={s.link} target="blank">
+          {data.user.name}
+        </a>
+      </div>
     </>
   );
 };
